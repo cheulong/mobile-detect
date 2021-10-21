eae22eb4cbdb2b81147c2cbb705565a9ec0409b7
Cheulong Sear <cheulong.sear@simyasolutions.com>
2021-10-21 13:04:06 +0700

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,48 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
-
+import { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import MobileDetect from "mobile-detect";
 function App() {
-  const [count, setCount] = useState(0)
+  const [mobile, setMobile] = useState("");
+  const [phone, setPhone] = useState("");
+  const [tablet, setTablet] = useState("");
+  const [userAgent, setUserAgent] = useState("");
+  const [os, setOs] = useState("");
+  const [isIPhone, setIsIPhone] = useState("");
+  const [isBot, setIsBot] = useState("");
+  const [versionWebkit, setVersionWebkit] = useState("");
+  const [versionStrBuild, setVersionStrBuild] = useState("");
 
+  useEffect(() => {
+    const md = new MobileDetect(navigator.userAgent);
+
+    // more typically we would instantiate with 'window.navigator.userAgent'
+    // as user-agent; this string literal is only for better understanding
+
+    setMobile(md.mobile()); // 'Sony'
+    setPhone(md.phone()); // 'Sony'
+    setTablet(md.tablet()); // null
+    setUserAgent(md.userAgent()); // 'Safari'
+    setOs(md.os()); // 'AndroidOS'
+    setIsIPhone(md.is("iPhone")); // false
+    setIsBot(md.is("bot")); // false
+    setVersionWebkit(md.version("Webkit")); // 534.3
+    setVersionStrBuild(md.versionStr("Build")); // '4.1.A.0.562'
+    return () => {};
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <p>mobile: {mobile}</p>
+      <p>phone: {phone}</p>
+      <p>tablet: {tablet}</p>
+      <p>userAgent: {userAgent}</p>
+      <p>os: {os}</p>
+      <p>isIPhone: {isIPhone}</p>
+      <p>isBot: {isBot}</p>
+      <p>versionWebkit: {versionWebkit}</p>
+      <p>versionStrBuild: {versionStrBuild}</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
